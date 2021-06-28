999b46c48f314370d78a55de91df055b03669c52
CGM - Guilherme <guilherme.tes@gmail.com>
2021-06-28 17:15:23 -0300

    adding statistcs

--- clubes.py
+++ clubes.py
@@ -20,18 +20,21 @@ notebook.pack(pady=2, expand=True)
 frame1 = inclusao.monta_formulario(notebook)
 frame2 = listagem.monta_grid(notebook)
 frame3 = controle_clubes.monta_grid_divisao(notebook)
-frame4 = controle_clubes.monta_labels(notebook)
+frame4 = estatistica.monta_labels(notebook)
 
 
 frame1.pack(fill='both', expand=True)
 frame2.pack(fill='both', expand=True)
 frame3.pack(fill='both', expand=True)
+frame4.pack(fill='both', expand=True)
+
 
 # add frames to notebook
 
 notebook.add(frame1, text='Inclusão de clubes')
 notebook.add(frame2, text='Listagem de clubes')
 notebook.add(frame3, text='Controle de clubes')
+notebook.add(frame4, text='Estatísticas')
 
 
 root.mainloop()
--- telas/estatistica.py
+++ telas/estatistica.py
@@ -5,38 +5,24 @@ import requests
 import locale
 locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8') 
 
-num = 0
+
 total = 0
 maior = 0
-carros_maiores = []
 destaques = 0
 clubes = None
 
 def dados():
-  global num, total, maior, destaques, carros, carros_maiores
-
-  num = 0
+  global total, destaques, clubes
   total = 0
-  maior = 0
-  carros_maiores = []
   destaques = 0
 
   URL_CLUBES = "http://localhost:3000/clubes"
   r = requests.get(url=URL_CLUBES)
   clubes = r.json()
-
-  for carro in carros:
-    num += 1
-    total += float(carro["preco"])
-    if float(carro["preco"]) > maior:
-      maior = float(carro["preco"])      
-    if carro["destaque"]:
+  total = len(clubes)
+  for clube in clubes:  
+    if clube["destaque"]:
       destaques += 1  
-
-  # percorre a lista para identificar os veículos de maior valor (pode ser mais que 1)
-  for carro in carros:
-    if float(carro["preco"]) == maior:
-      carros_maiores.append(carro["modelo"])
 
 def monta_labels(notebook):
 
@@ -44,19 +30,9 @@ def monta_labels(notebook):
 
   dados()
 
-  totalf = locale.currency(total, grouping=True, symbol=None)
-  media = total / num
-  mediaf = locale.currency(media, grouping=True, symbol=None)
-  maiorf = locale.currency(maior, grouping=True, symbol=None)
-
-  # Labels para exibir dados
-  ttk.Label(cadframe, text=f"Número de Veículos Cadastrados: {num}").grid(column=1, row=1, sticky=W)
-  ttk.Label(cadframe, text=f"Número de Veículos em Destaque: {destaques}").grid(column=1, row=2, sticky=W)
-  ttk.Label(cadframe, text=f"Total do Preço dos Veículos R$: {totalf}").grid(column=1, row=3, sticky=W)
-  ttk.Label(cadframe, text=f"Preço Médio dos Veículos R$: {mediaf}").grid(column=1, row=4, sticky=W)
-  ttk.Label(cadframe, text=f"Maior Preço de Veículo R$: {maiorf}").grid(column=1, row=5, sticky=W)
-  ttk.Label(cadframe, text=f"Veículo(s) de Maior Preço: {', '.join(carros_maiores)}").grid(column=1, row=6, sticky=W)
-
+  ttk.Label(cadframe, text=f"Número de Clubes Cadastrados: {total}").grid(column=1, row=1, sticky=W)
+  ttk.Label(cadframe, text=f"Número de Clubes em Destaque: {destaques}").grid(column=1, row=2, sticky=W)
+  
   for child in cadframe.winfo_children(): 
     child.grid_configure(padx=20, pady=10)    
 
